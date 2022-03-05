249e002c796f25072fe33b8b802f65f6edc44a4a
abhijeet-1001 <100982115+abhijeet-1001@users.noreply.github.com>
2022-03-05 10:41:48 +0530

    Update FirstCode.java
    Modified line no 2

--- FirstCode.java
+++ FirstCode.java
@@ -1,5 +1,5 @@
 This is my first code
-I am going to add this code to main branch
+I am going to add this code to main branch from NewBranch
 I am very excited to learn github
 Lets learn it with passion
 I have added this line from NewBranch
